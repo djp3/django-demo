fec4611d3d6fc55182e6a74f9a7851a4f85c2e44
Don Patterson <d_j_p_3@djp3.net>
2022-12-08 10:42:11 -0800

    Fix allowed hosts

--- django_demo_project/django_demo_project/settings.py
+++ django_demo_project/django_demo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+qe=j)vb_p@eg+m8_^fl8f!i&=l2he!g4m^_gtv90uh=ey_+!g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
